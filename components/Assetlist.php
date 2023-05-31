ffc4dd4dc913f5550cd79529bc5e982f53a3a815
NielsvandenDries <niels@openmindedit.nl>
2023-05-31 13:40:14 +0200

    update

---
+++ components/Assetlist.php
@@ -0,0 +1,27 @@
+<?php namespace Nielsvandendries\Assetmanagement\Components;
+
+use Cms\Classes\ComponentBase;
+
+/**
+ * Assetlist Component
+ *
+ * @link https://docs.octobercms.com/3.x/extend/cms-components.html
+ */
+class Assetlist extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'assetlist Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    /**
+     * @link https://docs.octobercms.com/3.x/element/inspector-types.html
+     */
+    public function defineProperties()
+    {
+        return [];
+    }
+}
