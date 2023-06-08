b3fd6ba6b702b8e18d98d50db1559ad59ea6ace5
NielsvandenDries <niels@openmindedit.nl>
2023-06-08 13:44:53 +0200

    update

--- components/Assetdetails.php
+++ components/Assetdetails.php
@@ -25,30 +25,16 @@ class Assetdetails extends ComponentBase
         ];
     }
 
+
     public function onRun()
     {
-        $assets = Asset::with('owner', 'user')->get()->toArray();
-        $this->page['assets'] = $assets;
-
-        $image = Asset::with('image')->first();
-    
-        $photoDetails = [];
-    
-        if ($image) {
-            $photoDetails = $image->photoDetails;
-        }
-    
-        $this->page['photoDetails'] = $photoDetails;
-
         $slug = $this->property('slug');
-        traceLog($slug); // Controleer de waarde van de slug
-        
         $asset = Asset::where('slug', $slug)->first();
-        traceLog($asset); // Controleer of de asset is gevonden
-        
-        if (!$asset) {
-            traceLog(Asset::all()->pluck('slug')); // Controleer de lijst van alle slugs in de database
-            traceLog('Asset not found'); // Voeg deze regel toe om te controleren of de asset niet wordt gevonden
+
+        if ($asset) {
+            $this->page['asset'] = $asset;
+        } else {
+            $this->page['asset'] = null;
         }
     }
 }
